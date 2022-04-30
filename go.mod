4ceaa02932d01fe95f1600196d3e3c1a0335d99a
lemon-mint <ice.lemons.mint@gmail.com>
2022-04-30 10:03:30 +0900

    Add Unlock

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/lemon-mint/golang-q-benchmark
 go 1.18
 
 require (
-	github.com/alphadose/zenq v0.0.0-20220427151350-4739710dbcee // indirect
-	github.com/lemon-mint/go-datastructures v1.1.0 // indirect
+	github.com/alphadose/zenq v0.0.0-20220427151350-4739710dbcee
+	github.com/lemon-mint/go-datastructures v1.1.0
+	github.com/lemon-mint/unlock v1.2.1
 )
--- main_test.go
+++ main_test.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"testing"
+	"unsafe"
 
 	"github.com/alphadose/zenq"
 	"github.com/lemon-mint/go-datastructures/queue"
+	"github.com/lemon-mint/unlock"
 )
 
 type User struct {
@@ -28,6 +30,38 @@ func BenchmarkZenQ(b *testing.B) {
 	)
 }
 
+func BenchmarkStdChan(b *testing.B) {
+	q := make(chan User, 4096)
+	b.RunParallel(
+		func(p *testing.PB) {
+			v0 := User{Name: "John", Age: 30}
+			for p.Next() {
+				q <- v0
+				v1 := <-q
+				if v1 != v0 {
+					b.Error("unexpected value")
+				}
+			}
+		},
+	)
+}
+
+func BenchmarkUnlock(b *testing.B) {
+	q := unlock.NewRingBuffer(4096)
+	b.RunParallel(
+		func(p *testing.PB) {
+			v0 := &User{Name: "John", Age: 30}
+			for p.Next() {
+				q.EnQueue(unsafe.Pointer(v0))
+				v1 := q.DeQueue()
+				if *(*User)(v1) != *v0 {
+					b.Error("unexpected value")
+				}
+			}
+		},
+	)
+}
+
 func BenchmarkGoDataStructures(b *testing.B) {
 	q := queue.NewRingBuffer[User](4096)
 	b.RunParallel(
@@ -42,22 +76,6 @@ func BenchmarkGoDataStructures(b *testing.B) {
 				if err != nil {
 					b.Error("unexpected error")
 				}
-				if v1 != v0 {
-					b.Error("unexpected value")
-				}
-			}
-		},
-	)
-}
-
-func BenchmarkStdChan(b *testing.B) {
-	q := make(chan User, 4096)
-	b.RunParallel(
-		func(p *testing.PB) {
-			v0 := User{Name: "John", Age: 30}
-			for p.Next() {
-				q <- v0
-				v1 := <-q
 				if v1 != v0 {
 					b.Error("unexpected value")
 				}
